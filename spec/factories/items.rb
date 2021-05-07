2b1766d21a30f5aab9eae04a67badc8a6b52610e
kohohsuke <82505121+kohohsuke@users.noreply.github.com>
2021-05-07 15:59:22 +0900

    商品出品機能の実装

---
+++ spec/factories/items.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :item do
+    name             { Faker::Name.initials(number: 5) }
+    description      { Faker::Lorem.sentences(number: 1) }
+    category_id      { 1 }
+    status_id        { 1 }
+    shipping_free_id { 1 }
+    prefecture_id    { 1 }
+    schedule_id      { 1 }
+    price            { Faker::Number.number(digits: 7) }
+    
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+  end
+end
