ea41fbd5ae4ae04e9e9501515be49925ae83c646
kohohsuke <82505121+kohohsuke@users.noreply.github.com>
2021-05-10 16:03:49 +0900

    Rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,11 +29,11 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-      if @item.update(item_params)
-        redirect_to root_path
-      else
-        render :edit
-      end
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -45,8 +45,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
