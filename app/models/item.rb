5bea3a91d72c0bdbd63765c7e943be93d2c4f4b7
kohohsuke <82505121+kohohsuke@users.noreply.github.com>
2021-05-14 15:46:11 +0900

    Rubocopによる修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,15 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+
   with_options presence: true do
     validates :image
     validates :name
     validates :description
-    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
-    validates :price, numericality: {
-      greater_than_or_equal_to: 300,
-      less_than_or_equal_to: 9_999_999,
-      message: 'Out of setting range'
-    }
+    validates :price
     with_options numericality: { other_than: 0, message: 'Select' } do
       validates :category_id
       validates :status_id
